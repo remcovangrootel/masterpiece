7dae3eb597046ab6c626d87f3ac05b3ad65ccc71
remcovangrootel <842520@student.fontys.nl>
2025-12-13 13:58:25 +0100

    Add files via upload

---
+++ hockey-app - versie 5 trainingen toevoegen en zaken verwijderen/front end/src/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+const api = axios.create({ baseURL: 'http://localhost:4000' });
+
+export const getPlayers = () => api.get('/players').then(r => r.data);
+export const addPlayer = (data) => api.post('/players', data).then(r => r.data);
+
+export const getTrainings = () => api.get('/trainings').then(r => r.data);
+export const addTraining = (data) => api.post('/trainings', data).then(r => r.data);
+
+export const addScore = (data) => api.post('/scores', data).then(r => r.data);
+export const getPlayerScores = (id) => api.get(`/scores/player/${id}`).then(r => r.data);
+export const getTeamScoresByTraining = (trainingId) => api.get(`/scores/team/${trainingId}`).then(r => r.data);
+export const getOverview = (metric) => api.get(`/scores/overview/${metric}`).then(r => r.data);
+export const deletePlayer = (id) => api.delete(`/players/${id}`).then(r => r.data);
+export const deleteTraining = (id) => api.delete(`/trainings/${id}`).then(r => r.data);
